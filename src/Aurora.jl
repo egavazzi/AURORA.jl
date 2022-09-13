1f33ed24ac2b4e0b194049d05cf17f12bb206a24
Etienne <etienne.gavazzi@uit.no>
2022-09-13 13:15:00 +0000

    add Crank-Nicolson scheme

--- src/Aurora.jl
+++ src/Aurora.jl
@@ -6,10 +6,12 @@ include("setup.jl")
 include("matrix_building.jl")
 include("phase_functions.jl")
 include("utilitaries.jl")
+include("Crank_Nicolson.jl")
 
 export setup
 export phase_fcn_N2, phase_fcn_O2, phase_fcn_O, convert_phase_fcn_to_3D
-export beams2beams, make_A, make_B
+export beams2beams, make_A, make_B, make_D
 export v_of_E
+export Crank_Nicolson, d2M
 
 end
