6f69b21fe49ba2697d33fb557cc374449091ef10
Etienne <etienne.gavazzi@uit.no>
2022-09-22 16:12:49 +0000

    add input_flux.jl , to load the incoming fluxes

--- src/Aurora.jl
+++ src/Aurora.jl
@@ -1,18 +1,21 @@
 module Aurora
 
 include("setup.jl")
-include("matrix_building.jl")
+include("input_flux.jl")
 include("phase_functions.jl")
 include("utilitaries.jl")
+include("matrix_building.jl")
 include("crank_nicolson.jl")
 include("cascading.jl")
 include("energy_degradation.jl")
 
 export setup
+export Ie_top_from_file
 export phase_fcn_N2, phase_fcn_O2, phase_fcn_O, convert_phase_fcn_to_3D
 export loss_to_thermal_electrons, beams2beams, make_A, make_B, make_D
-export v_of_E, cascading_N2, cascading_O2, cascading_O
+export v_of_E
 export Crank_Nicolson, d2M
-export update_Q!, update_Q2!
+export cascading_N2, cascading_O2, cascading_O
+export update_Q!
 
 end
---
+++ src/input_flux.jl
@@ -0,0 +1,25 @@
+using MAT
+
+function Ie_top_from_file(filename, μ_center, t, E, n_loop)
+    Ie_top = Array{Float64}(undef, length(μ_center), length(t), length(E))
+
+    file = matopen(filename)
+    Ie_top_raw = read(file, "Ie_total")
+    close(file)
+
+    # for constant input flux (e.g. first run), we need to resize the matrix from
+    # [1, n_μ * [n_E, 1]] to [1, n_μ * [n_E, n_t]]
+    if size(Ie_top[1], 2) == 1  
+        for i_μ in eachindex(μ_center)
+            Ie_top_raw[i_μ] = Ie_top_raw[i_μ] * ones(1, length(t) + (n_loop - 1) * (length(t) - 1)) # (e-/m²/s)
+        end
+    end
+
+    # then we resize the matrix from [1, n_μ * [n_E, n_t]] to [n_μ, n_t, n_E] to be consistent with
+    # the other flux matrices
+    for i_μ in eachindex(μ_center)
+        Ie_top[i_μ, :,  :] = Ie_top_raw[i_μ][1:length(E), :]' # (e-/m²/s)
+    end
+
+    return Ie_top
+end
