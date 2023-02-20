f92f230fb94be8d768c0c04114c93458083a6d94
Etienne <etienne.gavazzi@uit.no>
2023-02-20 19:26:48 +0100

    fix Matlab path in Plot_setup.jl

--- scripts/Plot_setup.jl
+++ scripts/Plot_setup.jl
@@ -3,15 +3,16 @@ using Aurora
 
 altitude_max = 400;
 θ_lims = 180:-10:0;
+path_to_AURORA_matlab = "/home/etienne/Documents/MATLAB/AURORA/"    # path to Matlab executable
 
 
 h_atm, ne, Te, E, dE,
     n_neutrals, E_levels_neutrals, σ_neutrals,
-    θ_lims, μ_lims, μ_center, μ_scatterings = setup(altitude_max, θ_lims, E_max);
+    θ_lims, μ_lims, μ_center, μ_scatterings = setup(path_to_AURORA_matlab, altitude_max, θ_lims, E_max);
 
 ## Plot densities
 f1 = Figure()
-ax11 = Axis(f1[1,1], xlabel = "(m⁻³)", ylabel = "height (km)", 
+ax11 = Axis(f1[1,1], xlabel = "(m⁻³)", ylabel = "height (km)",
           title = "MSIS neutral density", xscale = log10, xticks = LogTicks(LinearTicks(9)))
 lines!(n_neutrals.nN2, h_atm/1e3, linewidth = 2, label = "N₂")
 lines!(n_neutrals.nO2, h_atm/1e3, linestyle = :dash, linewidth = 2, color = :red, label = "O₂")
@@ -19,7 +20,7 @@ lines!(n_neutrals.nO, h_atm/1e3, linestyle = :dashdot, linewidth = 2, label = "O
 xlims!(1e12, 1e20)
 ylims!(85, 400)
 axislegend("Densities", position = :rt)
-ax12 = Axis(f1[1,2], xlabel = "(m⁻³)", ylabel = "height (km)", 
+ax12 = Axis(f1[1,2], xlabel = "(m⁻³)", ylabel = "height (km)",
         title = "electron density", xscale = log10)
 lines!(ne, h_atm/1e3, linewidth = 2)
 xlims!(1e8, 1e13)
