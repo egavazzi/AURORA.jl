8eb22bd73e3c9ff6d98d1d2a9d686273b7a920ff
Etienne <etienne.gavazzi@uit.no>
2023-11-06 18:19:49 +0000

    add short description of script

--- scripts/Calculate_densities.jl
+++ scripts/Calculate_densities.jl
@@ -1,3 +1,7 @@
+# Will converts e- flux `Ie` (#e⁻/m²/s) into number density `n_e` (#e⁻/m³).
+# - Ie : electron flux (#e⁻/m²/s), 3D array [n_beam * nz, nt, nE]
+# - n_e: electron density (#e⁻/m³), 3D array [nz, nt, nE]
+
 using AURORA
 
 directory_to_process = "Visions2/Alfven_536s_correct_msis_and_scattering"
