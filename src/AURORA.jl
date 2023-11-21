61b5ff3fb4cff67cbbea60d2937d75eb97d0e7b5
Etienne <etienne.gavazzi@uit.no>
2023-11-21 14:11:15 +0100

    export new functions

--- src/AURORA.jl
+++ src/AURORA.jl
@@ -4,6 +4,7 @@ include("../internal_data/data_electron/e_N2_cross_sections.jl")
 export e_N2elastic, e_N2rot0_2
 
 include("setup.jl")
+include("nrlmsis.jl")
 include("input_flux.jl")
 include("phase_functions.jl")
 include("utilitaries.jl")
@@ -13,10 +14,12 @@ include("cascading.jl")
 include("energy_degradation.jl")
 include("scattering.jl")
 export setup_new, make_altitude_grid, make_energy_grid, make_scattering_matrices
+export find_nrlmsis_file
 export Ie_top_from_file, Ie_top_flickering, Ie_top_constant
 export phase_fcn_N2, phase_fcn_O2, phase_fcn_O, convert_phase_fcn_to_3D
 export loss_to_thermal_electrons, beams2beams, make_A, make_B, make_D
-export v_of_E, CFL_criteria, mu_avg, beam_weight, save_parameters, save_results, f_smooth_transition
+export v_of_E, CFL_criteria, mu_avg, beam_weight, save_parameters, save_results, f_smooth_transition,
+        rename_if_exists
 export d2M, Crank_Nicolson, Crank_Nicolson_Optimized
 export cascading_N2, cascading_O2, cascading_O
 export update_Q!
