5c4ed4925e1b0b8c2c5cbe57da77e54d0052ba2e
Etienne <etienne.gavazzi@uit.no>
2023-12-05 08:56:56 +0000

    update Control_make_all.m

--- MATLAB_dependencies/Control_make_all.m
+++ MATLAB_dependencies/Control_make_all.m
@@ -11,8 +11,8 @@
 %       exemple: results_dir = "/mnt/data/etienne/Julia/AURORA.jl/data/Visions2/"
 
 
-results_dir = "/mnt/data/etienne/Julia/AURORA.jl/data/Visions2/"
-RunDirs = {'Alfven_536s_correct_msis'} 
+results_dir = "/mnt/data/etienne/Julia/AURORA.jl/data/CFL_simulations/"
+RunDirs = {'1keV_CFL-32'} 
 %%
 make_all_Ie_top     % precipitation-spectra extraction at top
 make_all_Ie_top_MI  % precipitation-spectra extraction at top and top-1
@@ -20,7 +20,8 @@ make_all_Ie_top_MI  % precipitation-spectra extraction at top and top-1
 make_all_Q_lambda   % excitation and ionization-rates calculations
 make_all_I_lambda   % column-emission calculations
 %%
-make_all_IQ_lambda_plots    % volume/column-emission plots
+% This has been translated to Julia
+% make_all_IQ_lambda_plots    % volume/column-emission plots
 %%
 cxmax = 9                 % maximum value for the colorbar(s)
 movies2make = [1 0 0 0 0] % choose animations to make
