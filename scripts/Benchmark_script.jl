a54fbd8a04bb1ee765c1c9741d19e01a9fac23c5
Etienne <etienne.gavazzi@uit.no>
2023-12-09 13:31:15 +0000

    increase samples in profile

--- scripts/Benchmark_script.jl
+++ scripts/Benchmark_script.jl
@@ -4,11 +4,11 @@ using AURORA
 ## Setting parameters
 altitude_max = 500;         # (km) top altitude of the ionosphere
 θ_lims = 180:-10:0;         # (°) angle-limits for the electron beams
-E_max = 3000;               # (eV) upper limit to the energy grid
+E_max = 7000;               # (eV) upper limit to the energy grid
 B_angle_to_zenith = 13;     # (°) angle between the B-field line and the zenith
 
 t_sampling = 0:0.001:0.01;  # (s) time-array over which data will be saved
-n_loop = 5;                 # number of loops to run
+n_loop = 1;                 # number of loops to run
 
 Nthreads = 20;      # number of threads to be used for calculations of the energy_degradation
                     # 6 threads seems to be optimal on my machine (12th Gen Intel© Core™ i7-12800HX × 16 processor)
@@ -27,7 +27,8 @@ iri_file = find_iri_file(
                     # if left empty, the folder will be named using the current date and time (ex: data/root_savedir/20221029-1058/...)
 
 root_savedir = "Benchmark"   # name for the root folder where data will be saved (data/root_savedir/)
-name_savedir = "500km_$(E_max)eV_0-05s(3)"   # name for the actual data folder of the current experiment (data/root_savedir/name_savedir/...)
+name_savedir = "500km_$(E_max)eV_0-01s_megaturbo"   # name for the actual data folder of the current experiment (data/root_savedir/name_savedir/...)
+# name_savedir = "TEST"   # name for the actual data folder of the current experiment (data/root_savedir/name_savedir/...)
 
 # test rename_if_exists!() here ?
 
@@ -64,16 +65,12 @@ INPUT_OPTIONS = (;input_type, IeE_tot, z₀, E_min, Beams, t0, t1);
 ## Run the simulation
 
 # First we run with dumb parameters to compile
-# precompiled = 0
-# if precompiled == 0
-    calculate_e_transport(altitude_max, θ_lims, 50, B_angle_to_zenith, 0:0.001:0.01, 1,
-        msis_file, iri_file, "Benchmark", "tmp", INPUT_OPTIONS, Nthreads, 64)
-    # precompiled = 1
-# end
+calculate_e_transport(altitude_max, θ_lims, 50, B_angle_to_zenith, 0:0.001:0.01, 1,
+    msis_file, iri_file, "Benchmark", "tmp", INPUT_OPTIONS, Nthreads, 64)
 
 # Then we profile
 using Profile
-Profile.init(n = 10^8, delay = 0.01)
+Profile.init(n = 10^9, delay = 0.01)
 @profile calculate_e_transport(altitude_max, θ_lims, E_max, B_angle_to_zenith, t_sampling, n_loop,
     msis_file, iri_file, root_savedir, name_savedir, INPUT_OPTIONS, Nthreads, CFL_number)
 
@@ -81,3 +78,7 @@ Profile.init(n = 10^8, delay = 0.01)
 using ProfileCanvas
 profile_file = pkgdir(AURORA, "data", root_savedir, name_savedir, "profile.html")
 ProfileCanvas.html_file(profile_file, Profile.fetch())
+
+# Run without profiling
+# calculate_e_transport(altitude_max, θ_lims, E_max, B_angle_to_zenith, t_sampling, n_loop,
+#     msis_file, iri_file, root_savedir, name_savedir, INPUT_OPTIONS, Nthreads, CFL_number)
