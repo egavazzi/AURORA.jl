290629c0569141960f50c53b8efb0ff5ad708f24
Etienne <etienne.gavazzi@uit.no>
2023-12-18 11:12:10 +0100

    add script to compare J

---
+++ scripts/analysing/Compare_J.jl
@@ -0,0 +1,52 @@
+using AURORA
+using MAT
+using GLMakie
+GLMakie.activate!()
+using CairoMakie
+# CairoMakie.activate!()
+
+directory_to_process1 = "CFL_simulations/7keV_CFL-128"
+directory_to_process2 = "CFL_simulations/7keV_CFL-32"
+directory_to_process3 = "CFL_simulations/7keV_CFL-256"
+
+
+
+## Load the J data (parallel current)
+full_path_to_directory = pkgdir(AURORA, "data", directory_to_process1)
+J_file = joinpath(full_path_to_directory, "J.mat")
+data = matread(J_file)
+J_up1 = vec(data["J_up"])
+J_down1 = vec(data["J_down"])
+IeEup1 = vec(data["IeEup"])
+IeEdown1 = vec(data["IeEdown"])
+t1 = vec(data["t"])
+
+## Load the J data (parallel current)
+full_path_to_directory = pkgdir(AURORA, "data", directory_to_process2)
+J_file = joinpath(full_path_to_directory, "J.mat")
+data = matread(J_file)
+J_up2 = vec(data["J_up"])
+J_down2 = vec(data["J_down"])
+IeEup2 = vec(data["IeEup"])
+IeEdown2 = vec(data["IeEdown"])
+t2 = vec(data["t"])
+## Load the J data (parallel current)
+full_path_to_directory = pkgdir(AURORA, "data", directory_to_process3)
+J_file = joinpath(full_path_to_directory, "J.mat")
+data = matread(J_file)
+J_up3 = vec(data["J_up"])
+J_down3 = vec(data["J_down"])
+IeEup3 = vec(data["IeEup"])
+IeEdown3 = vec(data["IeEdown"])
+t3 = vec(data["t"])
+
+
+##
+f = Figure()
+ax = Axis(f[1, 1], xlabel = "time (s)", ylabel = "")
+sc1 = scatterlines!(ax, t, IeEup1)
+sc2 = scatterlines!(ax, t, IeEup2)
+sc3 = scatterlines!(ax, t, IeEup3)
+axislegend(ax, [sc1, sc2, sc3], ["128", "32", "256"], "CFL", position=:rb)
+DataInspector(f)
+f
