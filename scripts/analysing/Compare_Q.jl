fa0dae6eb435847b9cb2b25915e32244e0d0d196
Etienne <etienne.gavazzi@uit.no>
2023-12-18 11:13:02 +0100

    change comparisons to scatterlines and add DataInspector

--- scripts/analysing/Compare_Q.jl
+++ scripts/analysing/Compare_Q.jl
@@ -1,11 +1,14 @@
 using AURORA
 using MAT
 using CairoMakie
-CairoMakie.activate!(type = "png")
+# CairoMakie.activate!(type = "png")
+using GLMakie
+GLMakie.activate!(type = "png")
 
 
 directory_to_process1 = "CFL_simulations/7keV_CFL-128"
 directory_to_process2 = "CFL_simulations/7keV_CFL-32"
+directory_to_process3 = "CFL_simulations/7keV_CFL-256"
 
 
 
@@ -34,7 +37,18 @@ QO1D_2 = data["QO1D"]
 QO1S_2 = data["QO1S"]
 h_atm = vec(data["h_atm"]) ./ 1e3    # convert to km
 t = vec(data["t"])
-
+## Load the Q data (volume emission-rates)
+full_path_to_directory = pkgdir(AURORA, "data", directory_to_process3)
+Q_file = joinpath(full_path_to_directory, "Qzt_all_L.mat")
+data = matread(Q_file)
+Q4278_3 = data["Q4278"]
+Q6730_3 = data["Q6730"]
+Q7774_3 = data["Q7774"]
+Q8446_3 = data["Q8446"]
+QO1D_3 = data["QO1D"]
+QO1S_3 = data["QO1S"]
+h_atm = vec(data["h_atm"]) ./ 1e3    # convert to km
+t = vec(data["t"])
 
 ##
 diff_4278 = log10.(abs.(Q4278_1 .- Q4278_2))
@@ -116,10 +130,9 @@ f = with_theme(
     Theme(
         Axis = (
             xticksmirrored = true, yticksmirrored = false, xminorticksvisible = true,
-            yminorticksvisible = true,
+            yminorticksvisible = true, limits = ((0, 0.05), nothing)
             ),
-        Colorbar = (flip_vertical_label = true, vertical = true),
-        Heatmap = (rasterize = true,),
+        ScatterLines = (marker = :cross,),
         )
     ) do
 
@@ -128,21 +141,18 @@ f = with_theme(
 
     data1 = Q4278_1[idx_z, :]; data1[data1 .== 0] .= NaN
     data2 = Q4278_2[idx_z, :]; data2[data2 .== 0] .= NaN
+    data3 = Q4278_3[idx_z, :]; data3[data2 .== 0] .= NaN
 
     f = Figure(size = (1000, 800))
     ga = f[1, 1] = GridLayout()
-    ax4278 = Axis(ga[1, 1]; title = "4278 Å", xlabel = "t (s)", ylabel = "altitude (km)", yscale = log10)
-    l4278_1 = lines!(t, data1; rasterize = true, label = "CFL 128")
-    l4278_2 = lines!(t, data2; rasterize = true, label = "CFL 32")
+    ax4278 = Axis(ga[1, 1]; title = "4278 Å", xlabel = "t (s)", ylabel = "photons/m³/s", yscale = identity)
+    l4278_1 = scatterlines!(t, data1; rasterize = true, label = "CFL 128")
+    l4278_2 = scatterlines!(t, data2; rasterize = true, label = "CFL 32", marker = :cross)
+    l4278_3 = scatterlines!(t, data3; rasterize = true, label = "CFL 256")
     axislegend(ax4278; position = :rb)
-    # hm4278 = heatmap!(t, h_atm, diff_4278'; rasterize = true, colorrange = [0, 0.01])
-    # cb4278 = Colorbar(ga[1, 2], hm4278; label = "photons/m³/s")
-    # ylims!(nothing, 200)
-    # xlims!(0, 0.01)
-    colgap!(ga, 10)
-
 
     return f
 end
 
+DataInspector(f)
 f
