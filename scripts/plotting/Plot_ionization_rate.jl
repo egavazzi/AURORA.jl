73cbc497bc46362cf6e4dc334dd919e349e59ccc
Etienne <etienne.gavazzi@uit.no>
2024-04-15 14:39:08 +0200

    add plot of ionization rate as heatmap

--- scripts/plotting/Plot_ionization_rate.jl
+++ scripts/plotting/Plot_ionization_rate.jl
@@ -4,26 +4,22 @@ using GLMakie
 GLMakie.activate!()
 
 
+## Path to the data
+full_path_to_directory = joinpath(REVONTULI_MOUNT, "mnt/data/etienne/Julia/AURORA.jl/data/Visions2/Alfven_536s_lr_Bz-9_newZ_550km_finer-theta_halfstepsAB")
 
-
-# full_path_to_directory = joinpath(REVONTULI_MOUNT, "mnt/data/etienne/Julia/AURORA.jl/data/steady_state/test_AB-half-steps")
-full_path_to_directory = joinpath(REVONTULI_MOUNT, "mnt/data/etienne/Julia/AURORA.jl/data/Visions2/Alfven_536s")
-# pkgdir(AURORA, "data", "Visions2/Alfven_536s_correct_msis_and_scattering")
-
-
-
-## Plot Ionization rate
-# Read the data
+## Read the data
 ionization_file = joinpath(full_path_to_directory, "Qzt_all_L.mat")
 data = matread(ionization_file)
-QO2i = data["QO2i"] .* 1e4 # the Q were calculated from Ie in m⁻², but we have Ie in cm⁻², so we need to multiply by 1e4
+QO2i = data["QO2i"] .* 1e4 # Q was calculated with Ie in /cm² when it sould have been in /m², so we fix it here by multiplying with 1e4
 QOi = data["QOi"] .* 1e4
 QN2i = data["QN2i"] .* 1e4
+Qtot = QN2i + QO2i + QOi
 h_atm = vec(data["h_atm"])
-t = data["t"]
+t = vec(data["t"])
 
 
-## Plot ionization rates
+## Plot Ionization rate as an animated lineplot
+#region
 fig = Figure(size=(800, 1200))
 # make slider to show and control i_t
 sl_time = Slider(fig[2, 1], range = 1:length(t), startvalue = 1)
@@ -33,7 +29,7 @@ time = Observable(string(round(t[i_t[]], digits=3)) * "s")
 QO2i_slice = Observable(QO2i[:, i_t[]])
 QOi_slice = Observable(QOi[:, i_t[]])
 QN2i_slice = Observable(QN2i[:, i_t[]])
-Q_tot = Observable(QO2i_slice[] + QOi_slice[] + QN2i_slice[])
+Qtot_slice = Observable(QO2i_slice[] + QOi_slice[] + QN2i_slice[])
 # plot ionization rate
 ax1 = Axis(fig[1, 1], title = time, xlabel = "Ionization rate (/m³/s)", ylabel = "altitude (km)",
     yminorticksvisible = false, yminorgridvisible = false, yticks = 100:100:600,
@@ -46,7 +42,7 @@ ylims!(h_atm[1] / 1e3 - 30, h_atm[end] / 1e3 + 30)
 l_QO2i = lines!(QO2i_slice, h_atm / 1e3; linestyle =:dash)
 l_QOi = lines!(QOi_slice, h_atm / 1e3; linestyle =:dash)
 l_QN2i = lines!(QN2i_slice, h_atm / 1e3; linestyle =:dash)
-l_tot = lines!(Q_tot, h_atm / 1e3; color = :black)
+l_tot = lines!(Qtot_slice, h_atm / 1e3; color = :black)
 Legend(fig[1, 2], [l_QO2i, l_QOi, l_QN2i, l_tot], ["O2", "O", "N2", "tot"], "Ionization rates")
 # make button to run/stop the animation
 fig[3, 1] = buttongrid = GridLayout(tellheight = true, tellwidth = false)
@@ -69,32 +65,45 @@ on(reset.clicks) do clicks;
     Makie.set_close_to!(sl_time, sl_time.value[])
 end
 # make function to step in time
-function step!(QO2i_slice, QOi_slice, QN2i_slice, Q_tot, time, i_t)>
+function step!(QO2i_slice, QOi_slice, QN2i_slice, Qtot_slice, time, i_t)>
     QO2i_slice[] = QO2i[:, i_t[]]
     QOi_slice[] = QOi[:, i_t[]]
     QN2i_slice[] = QN2i[:, i_t[]]
-    Q_tot[] = QO2i_slice[] + QOi_slice[] + QN2i_slice[]
+    Qtot_slice[] = QO2i_slice[] + QOi_slice[] + QN2i_slice[]
     time[] = string(round(t[i_t], digits=3)) * "s"
 end
 onany(i_t) do i_t
-    step!(QO2i_slice, QOi_slice, QN2i_slice, Q_tot, time, i_t[])
+    step!(QO2i_slice, QOi_slice, QN2i_slice, Qtot_slice, time, i_t[])
 end
 # and display
-display(fig)
-
-
-
-
-
-##
-savefile = joinpath(full_path_to_directory, "Qz.png")
-save(savefile, fig)
-
-
-
-
-##
+# display(fig)
+display(GLMakie.Screen(), fig)
+## Save as a video
 video_file = joinpath(full_path_to_directory, "Ionization_rate.mp4")
 @time record(fig, video_file, 1:length(t); framerate = 60) do i_t
     Makie.set_close_to!(sl_time, i_t)
 end
+## Save a snapshot
+savefile = joinpath(full_path_to_directory, "Ionization_rate.png")
+save(savefile, fig)
+#endregion
+
+
+
+
+
+## Plot Ionization rate as heatmap
+#region
+fig = Figure(size = (1200, 1000))
+ax = Axis(fig[1, 1], xlabel = "t (s)", ylabel = "altitude (km)", aspect= AxisAspect(1))
+hm = heatmap!(t, h_atm / 1e3, log10.(Qtot)'; colorrange = (6, 10))
+custom_formatter(values) = map(v -> rich("10", superscript("$(round(Int64, v))")), values)
+cb = Colorbar(fig[1, 2], hm; tickformat = custom_formatter, label = "Ionization rate (/m³/s)")
+Qtot_contour = log10.(Qtot) |> (x -> replace(x, -Inf => 0))
+ct = contour!(t, h_atm / 1e3, Qtot_contour'; levels = 6:10, color = :black, labels = true)
+display(GLMakie.Screen(), fig)
+
+# Save the figure
+savefile = joinpath(full_path_to_directory, "Ionization_rate_zt.png")
+save(savefile, fig)
+#endregion
