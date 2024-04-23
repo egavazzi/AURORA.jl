a782d2a6d44286d6f7e71a6818d29acd2ae8dc50
OS <oli.st@live.com>
2024-04-23 13:40:52 +0000

    minor cleanup

--- scripts/Control_script_SteadyState.jl
+++ scripts/Control_script_SteadyState.jl
@@ -1,6 +1,6 @@
 ##
-ENV["JULIA_CONDAPKG_BACKEND"] = "Null"
-ENV["JULIA_PYTHONCALL_EXE"] = "/usr/local/bin/python3"  # optional
+#ENV["JULIA_CONDAPKG_BACKEND"] = "Null"
+#ENV["JULIA_PYTHONCALL_EXE"] = "/usr/local/bin/python3"  # optional
 #ENV["JULIA_PYTHONCALL_EXE"] = "@PyCall"  # optional
 
 using AURORA
@@ -15,9 +15,9 @@ iri_file = find_iri_file(
     );
 
 
-root_savedir = "ionprod"   
+root_savedir = "ionprod"  
 # name for the root folder where data will be saved (data/root_savedir/)
-name_savedir = "1e5keV"   
+#name_savedir = "1e5keV"   
 # name for the actual data folder of the current experiment (data/root_savedir/name_savedir/...)
 
 
@@ -34,7 +34,6 @@ z₀ = altitude_max;          # (km) altitude of the source
 Beams = 1:length(θ_lims)-1;   # beam numbers for the precipitation, starting with field aligned down
 t0 = 0;                     # (s) time of start for smooth transition
 t1 = 0;                     # (s) time of end for smooth transition
-INPUT_OPTIONS = (;input_type, IeE_tot, z₀, E_min, Beams, t0, t1);
 
 
 
@@ -44,6 +43,7 @@ for i in 1:length(e_grid)-1
     E_max = e_grid[i+1]
     E_min = e_grid[i]
     name_savedir = string((E_min + E_max)/2) * "eV"
+    INPUT_OPTIONS = (;input_type, IeE_tot, z₀, E_min, Beams, t0, t1);
     calculate_e_transport_steady_state(altitude_max, θ_lims, E_max, B_angle_to_zenith,
         msis_file, iri_file, root_savedir, name_savedir, INPUT_OPTIONS)
 end
