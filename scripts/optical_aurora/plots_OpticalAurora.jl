b3f2224de49597cc15129c9e9ec80cb583f16d0a
Etienne <etienne.gavazzi@uit.no>
2024-07-11 16:35:16 +0200

    add plot to check effect of dz

--- scripts/optical_aurora/plots_OpticalAurora.jl
+++ scripts/optical_aurora/plots_OpticalAurora.jl
@@ -1,8 +1,6 @@
 #=
 This is a script to produce figures related to the optical aurora task
 =#
-
-
 using AURORA
 using MAT
 # using CairoMakie
@@ -37,6 +35,7 @@ t = 1
 
 
 ## Plots comparing total ionization profiles for different dz
+#region
 f = Figure(size = (1000, 1000))
 ax = Axis(f[1, 1]; xscale = log10, limits = ((1e6, 1e14), (90, 200)),
           xminorgridvisible = true, xminorticks = IntervalsBetween(9),
@@ -66,7 +65,7 @@ l_O_150m = lines!(vec(QOi) .+ 1e-10, h_atm; color=Cycled(4), linestyle=:dot)
 Leg = Legend(f[1, 2], [[l_tot_75m, l_N2_75m, l_O2_75m, l_O_75m], [l_tot_100m, l_N2_100m, l_O2_100m, l_O_100m]],
                 [["tot", "N2", "O2", "O"], ["tot", "N2", "O2", "O"]],
                 [["dz = 25m"], ["dz = 15m"]])
-
+#endregion
 
 
 
@@ -141,6 +140,7 @@ Some notes
 - The factor 2.3 is because I accidentaly used two different total energy flux for the 1keV
 and 3keV precipitation spectra when it should have been exactly the same total energy flux.
 =#
+#region
 # 1st simulation to compare
 full_path_to_directory = "data/Optical_Aurora_course/Maxwellian_3kev_with_LET_dzchangefrom200km"
 # Read the ionization data
@@ -215,3 +215,99 @@ l_O1D_1keV_G = lines!(vec(QO1D) .+ 1e-10, h_atm; color=Cycled(2), linestyle=:das
 Leg = Legend(f[1, 2], [[l_O1D_3keV_M, l_O1D_3keV_G], [l_O1D_1keV_M, l_O1D_1keV_G]],
                 [["Maxwellian ", "Gaussian "], ["Maxwellian ", "Gaussian "]],
                 [["3 keV"], ["1 keV"]])
+#endregion
+
+
+
+
+## Checking dz inhomogeneity by plotting profiles of Ie[:, :, iE]
+dzchange = 130
+file_to_read = "/home/etienne/Documents/Julia/AURORA.jl/data/Optical_Aurora_course/Maxwellian_3kev_with_LET_dzchangefrom130km/IeFlickering-01.mat"
+data = matread(file_to_read)
+μ_lims = data["mu_lims"]
+h_atm = data["h_atm"]
+E = data["E"]
+dE = diff(E); dE = [dE; dE[end]]
+Ie = data["Ie_ztE"]
+# Resize Ie from [n_μ x n_z, 1, n_E] to [n_μ, n_z, n_E]
+Ie_restructured = zeros(length(μ_lims) - 1, length(h_atm), length(E))
+for i in 1:(length(μ_lims) - 1)
+    Ie_restructured[i, :, :] .= Ie[(1:length(h_atm)) .+ (i - 1) * length(h_atm), 1, :]
+end
+# Plot
+f = Figure(size = (1800, 1000))
+ax = Axis(f[1, 1]; xscale = log10)
+iE = findmin(abs.(E .- 10000))[2]
+l = lines!(ax, Ie_restructured[1, :, iE], h_atm / 1e3)
+iE = findmin(abs.(E .- 5000))[2]
+l = lines!(ax, Ie_restructured[1, :, iE], h_atm / 1e3)
+iE = findmin(abs.(E .- 3000))[2]
+l = lines!(ax, Ie_restructured[1, :, iE], h_atm / 1e3)
+iE = findmin(abs.(E .- 1000))[2]
+l = lines!(ax, Ie_restructured[1, :, iE], h_atm / 1e3)
+iE = findmin(abs.(E .- 500))[2]
+l = lines!(ax, Ie_restructured[1, :, iE], h_atm / 1e3)
+xlims!(ax, 1e-4, 1e1)
+Legend(f[1, 2], ax.scene.plots, ["10 keV", "5 keV", "3 keV", "1 keV", "500 eV"])
+hlines!(dzchange; color = :black, linestyle = :dash)
+text!(1e-4, dzchange, text = "$dzchange km", align = (:left, :bottom), offset = (10, 0))
+display(GLMakie.Screen(), f)
+
+
+
+
+## Checking dz inhomogeneity by plotting profiles of Ie[:, :, iE], comparing simulations
+# Load 1st simulation
+file_to_read = "/home/etienne/Documents/Julia/AURORA.jl/data/Optical_Aurora_course/tests/test_constant_onset_20keV_zmin90km_nohalfsteps_dz150m-from150km/IeFlickering-01.mat"
+data = matread(file_to_read)
+μ_lims = data["mu_lims"]
+h_atm = data["h_atm"]
+E = data["E"]
+dE = diff(E); dE = [dE; dE[end]]
+Ie = data["Ie_ztE"]
+# Resize Ie from [n_μ x n_z, 1, n_E] to [n_μ, n_z, n_E]
+Ie_restructured = zeros(length(μ_lims) - 1, length(h_atm), length(E))
+for i in 1:(length(μ_lims) - 1)
+    Ie_restructured[i, :, :] .= Ie[(1:length(h_atm)) .+ (i - 1) * length(h_atm), 1, :]
+end
+# Plot
+f = Figure(size = (1800, 1000))
+ax = Axis(f[1, 1]; xscale = log10)
+iE = findmin(abs.(E .- 20000))[2]
+l1 = lines!(ax, Ie_restructured[1, :, iE], h_atm / 1e3)
+iE = findmin(abs.(E .- 10000))[2]
+l2 = lines!(ax, Ie_restructured[1, :, iE], h_atm / 1e3)
+iE = findmin(abs.(E .- 5000))[2]
+l3 = lines!(ax, Ie_restructured[1, :, iE], h_atm / 1e3)
+iE = findmin(abs.(E .- 1000))[2]
+l4 = lines!(ax, Ie_restructured[1, :, iE], h_atm / 1e3)
+xlims!(ax, 1e-4, 1e1)
+
+# Load 2nd simulation
+file_to_read = "/home/etienne/Documents/Julia/AURORA.jl/data/Optical_Aurora_course/tests/test_constant_onset_20keV_zmin90km_nohalfsteps_dz25m-from150km/IeFlickering-01.mat"
+data = matread(file_to_read)
+μ_lims = data["mu_lims"]
+h_atm = data["h_atm"]
+E = data["E"]
+dE = diff(E); dE = [dE; dE[end]]
+Ie = data["Ie_ztE"]
+# Resize Ie from [n_μ x n_z, 1, n_E] to [n_μ, n_z, n_E]
+Ie_restructured = zeros(length(μ_lims) - 1, length(h_atm), length(E))
+for i in 1:(length(μ_lims) - 1)
+    Ie_restructured[i, :, :] .= Ie[(1:length(h_atm)) .+ (i - 1) * length(h_atm), 1, :]
+end
+# Plot
+iE = findmin(abs.(E .- 20000))[2]
+lines!(ax, Ie_restructured[1, :, iE], h_atm / 1e3; color = l1.color, linestyle = :dashdot)
+iE = findmin(abs.(E .- 10000))[2]
+lines!(ax, Ie_restructured[1, :, iE], h_atm / 1e3; color = l2.color, linestyle = :dashdot)
+iE = findmin(abs.(E .- 5000))[2]
+lines!(ax, Ie_restructured[1, :, iE], h_atm / 1e3; color = l3.color, linestyle = :dashdot)
+iE = findmin(abs.(E .- 1000))[2]
+lines!(ax, Ie_restructured[1, :, iE], h_atm / 1e3; color = l4.color, linestyle = :dashdot)
+Legend(f[1, 2], ax.scene.plots, ["20 keV - dz 150m", "10 keV - dz 150m", "5 keV - dz 150m", "1 keV - dz 150m",
+                                 "20 keV - dz 25m", "10 keV - dz 25m", "5 keV - dz 25m", "1 keV - dz 25m"])
+display(GLMakie.Screen(), f)
+
+xlims!(1e4, 1e11)
+ylims!(80, 200)
