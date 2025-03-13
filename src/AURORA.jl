55527ec2f5ae7cdac9aafdf4eb652d530f84cfb7
Etienne <etienne.gavazzi@uit.no>
2025-03-13 18:08:43 +0100

    stop exporting cross-section functions

--- src/AURORA.jl
+++ src/AURORA.jl
@@ -2,17 +2,8 @@ module AURORA
 
 # AURORA functions
 include("../internal_data/data_electron/e_N2_cross_sections.jl")
-export e_N2elastic, e_N2rot0_2, e_N2rot0_4, e_N2rot0_6, e_N2rot0_8, e_N2vib0_1, e_N2vib0_2,
-    e_N2vib0_3, e_N2vib0_4, e_N2vib0_5, e_N2vib0_6, e_N2vib0_7, e_N2a3sup, e_N2b3pg,
-    e_N2w3du, e_N2bp3sum, e_N2ap1sum, e_N2w1du, e_N2e3sgp, e_N2ab1sgp, e_N2a1pg, e_N2c3pu,
-    e_N2bp1sup, e_N2cp1sup, e_N2cp3pu, e_N2d3sup, e_N2f3pu, e_N2g3pu, e_N2M1M2, e_N2o1pu,
-    e_N2dissociation, e_N2ionx2sgp, e_N2iona2pu, e_N2ionb2sup, e_N2dion, e_N2ddion
 include("../internal_data/data_electron/e_O2_cross_sections.jl")
-export e_O2elastic, e_O2_OO3S, e_O2_9p97, e_O2_8p4, e_O2_6, e_O2_4p5, e_O2b1Sgp, e_O2a1Dg,
-    e_O2vib, e_O2ionx2pg, e_O2iona4pu, e_O2ion16p9, e_O2ionb4sgm, e_O2dion, e_O2ddion
 include("../internal_data/data_electron/e_O_cross_sections.jl")
-export e_Oelastic, e_O1D, e_O1S, e_O3s5S0, e_O3s3S0, e_O3p5P, e_O3sp3D0, e_O3p3P, e_Oion4S0,
-    e_Oion2D0, e_Oion2P0, e_Oionion
 include("../internal_data/data_electron/emission_cross_sections.jl")
 
 include("setup.jl")
