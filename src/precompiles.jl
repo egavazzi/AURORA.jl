5f050deeaa2e21de4efe4b97b7a60a750e76a327
Etienne Gavazzi <etienne.gavazzi@uit.no>
2025-03-13 19:45:19 +0100

    Precompile the scattering functions (#52)
    * precompile the most-used scattering functions
    
    * update CHANGELOG.md

--- src/precompiles.jl
+++ src/precompiles.jl
@@ -3,8 +3,13 @@ using PrecompileTools
 let
     @setup_workload begin
         E, dE = make_energy_grid(3000)
+        θ_lims = 180:-45:0
         @compile_workload begin
             AURORA.load_cross_sections(E, dE)
+            AURORA.load_scattering_matrices(θ_lims, 10; verbose = false)
+            AURORA.cosd.(θ_lims);
+            AURORA.mu_avg(θ_lims);
+            AURORA.beam_weight(θ_lims);
         end
     end
 end
