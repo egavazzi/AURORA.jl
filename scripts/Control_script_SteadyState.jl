038c64fc16482e97920248abc6716549506ac091
Etienne Gavazzi <etienne.gavazzi@uit.no>
2025-04-23 15:55:31 +0200

    Save results anywhere (#57)
    * add function to create savedir path
    
    * add missing kwarg
    
    * remove outdated comment
    
    * fix custom behavior,  improve docstring
    
    * give savedir as input to the main functions
    
    * update control scripts templates
    
    * Update CHANGELOG.md

--- scripts/Control_script_SteadyState.jl
+++ scripts/Control_script_SteadyState.jl
@@ -1,26 +1,24 @@
-##
 using AURORA
-using BenchmarkTools
-using MAT
 
-altitude_max = 600;         # (km) top altitude of the ionosphere
-θ_lims = 180:-10:0;         # (°) angle-limits for the electron beams
+## Setting parameters
+altitude_max = 500;         # (km) top altitude of the ionosphere
+θ_lims = 180:-10:10;         # (°) angle-limits for the electron beams
 E_max = 3000;               # (eV) upper limit to the energy grid
 B_angle_to_zenith = 13;     # (°) angle between the B-field line and the zenith
 
 msis_file = find_nrlmsis_file(
-    year=2005, month=10, day=8, hour=22, minute=0, lat=70, lon=19, height=85:1:700
+    year=2004, month=10, day=8, hour=22, minute=0, lat=70, lon=19, height=85:1:700
     );
 iri_file = find_iri_file(
-    year=2005, month=10, day=8, hour=22, minute=0, lat=70, lon=19, height=85:1:700
+    year=2004, month=10, day=8, hour=22, minute=0, lat=70, lon=19, height=85:1:700
     );
 
+## Define where to save the results
+root_savedir = ""   # name of the root folder
+name_savedir = ""   # name of the experiment folder
+savedir = make_savedir(root_savedir, name_savedir)
 
-root_savedir = ""   # name for the root folder where data will be saved (data/root_savedir/)
-name_savedir = ""   # name for the actual data folder of the current experiment (data/root_savedir/name_savedir/...)
-
-
-
+## Define input parameters
 input_type = "constant_onset"
 IeE_tot = 1e-2;             # (W/m²) total energy flux of the FAB
 z₀ = altitude_max;          # (km) altitude of the source
@@ -30,6 +28,12 @@ t0 = 0;                     # (s) time of start for smooth transition
 t1 = 0;                     # (s) time of end for smooth transition
 INPUT_OPTIONS = (;input_type, IeE_tot, z₀, E_min, Beams, t0, t1);
 
-
+## Run the simulation
 calculate_e_transport_steady_state(altitude_max, θ_lims, E_max, B_angle_to_zenith,
-    msis_file, iri_file, root_savedir, name_savedir, INPUT_OPTIONS)
+    msis_file, iri_file, savedir, INPUT_OPTIONS);
+
+## Analyze the results
+make_Ie_top_file(savedir)
+make_volume_excitation_file(savedir)
+make_current_file(savedir)
+# make_column_excitation_file(savedir) -- does not make sense for steady-state
