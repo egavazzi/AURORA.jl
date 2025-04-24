19a35425f2fd2fa0281f0714a02eabaa666bf656
Etienne <etienne.gavazzi@uit.no>
2025-04-24 19:32:37 +0200

    modify the time-dependent template

--- scripts/Control_script.jl
+++ scripts/Control_script.jl
@@ -1,13 +1,13 @@
 using AURORA
 
 ## Setting parameters
-altitude_max = 600;         # (km) top altitude of the ionosphere
-θ_lims = 180:-10:0          # (°) angle-limits for the electron beams
-E_max = 3000;               # (eV) upper limit to the energy grid
-B_angle_to_zenith = 13;     # (°) angle between the B-field line and the zenith
+altitude_lims = [100, 600];     # (km) altitude limits of the ionosphere
+θ_lims = 180:-10:0              # (°) angle-limits for the electron beams
+E_max = 3000;                   # (eV) upper limit to the energy grid
+B_angle_to_zenith = 13;         # (°) angle between the B-field line and the zenith
 
-t_sampling = 0:0.001:0.1;   # (s) time-array over which data will be saved
-n_loop = 10;                # number of loops to run
+t_sampling = 0:0.001:0.1;       # (s) time-array over which data will be saved
+n_loop = 10;                    # number of loops to run
 
 CFL_number = 128;
 
@@ -19,9 +19,9 @@ iri_file = find_iri_file(
     );
 
 ## Define where to save the results
-root_savedir = "/home/etienne/Documents"   # name of the root folder
-name_savedir = "test_to_delete"   # name of the experiment folder
-savedir = make_savedir(root_savedir, name_savedir; behavior = "custom")
+root_savedir = ""   # name of the root folder
+name_savedir = ""   # name of the experiment folder
+savedir = make_savedir(root_savedir, name_savedir)
 
 ## Define input parameters
 # input_type = "from_file"
@@ -30,24 +30,24 @@ savedir = make_savedir(root_savedir, name_savedir; behavior = "custom")
 
 input_type = "flickering";
 IeE_tot = 1e-2;             # (W/m²) total energy flux of the FAB
-z₀ = 3000;                   # (km) altitude of the source
+z₀ = 3000;                  # (km) altitude of the source
 E_min = 100;                # (eV) bottom energy of the FAB
-f = 5;                     # (Hz) frequence of the modulation
-Beams = 1;                # beam numbers for the precipitation, starting with field aligned down
+f = 5;                      # (Hz) frequence of the modulation
+Beams = 1;                  # beam numbers for the precipitation, starting with field aligned down
 modulation = "sinus";       # type of the modulation ("square" or "sinus")
 INPUT_OPTIONS = (;input_type, IeE_tot, z₀, E_min, f, Beams, modulation);
 
 # input_type = "constant_onset"
 # IeE_tot = 1e-2;             # (W/m²) total energy flux of the FAB
-# z₀ = altitude_max;          # (km) altitude of the source
-# E_min = E_max - 100;         # (eV) bottom energy of the FAB
+# z₀ = altitude_lims[2];      # (km) altitude of the source
+# E_min = E_max - 100;        # (eV) bottom energy of the FAB
 # Beams = 1:2;                # beam numbers for the precipitation, starting with field aligned down
 # t0 = 0;                     # (s) time of start for smooth transition
 # t1 = 0;                     # (s) time of end for smooth transition
 # INPUT_OPTIONS = (;input_type, IeE_tot, z₀, E_min, Beams, t0, t1);
 
 ## Run the simulation
-calculate_e_transport(altitude_max, θ_lims, E_max, B_angle_to_zenith, t_sampling, n_loop,
+calculate_e_transport(altitude_lims, θ_lims, E_max, B_angle_to_zenith, t_sampling, n_loop,
     msis_file, iri_file, savedir, INPUT_OPTIONS, CFL_number)
 
 ## Run the analysis
