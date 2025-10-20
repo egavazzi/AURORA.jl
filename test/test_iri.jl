5ed682c7e6c03d91f000ad46072d0be99caf1335
Etienne <etienne.gavazzi@uit.no>
2025-10-20 17:07:28 +0200

    fix tests not running

--- test/test_iri.jl
+++ test/test_iri.jl
@@ -1,4 +1,4 @@
-@testitems "Search for file that does not exist" begin
+@testitem "Search for file that does not exist" begin
     PARAMETERS = [2009, 12, 10, 4, 5, 76, 5, 82:1:600]
     file_to_load = search_existing_iri_file(;
                                             year = PARAMETERS[1],
@@ -13,7 +13,7 @@
     @test isnothing(file_to_load)
 end
 
-@testitems "Search for file that already exists" begin
+@testitem "Search for file that already exists" begin
     PARAMETERS = [2009, 12, 10, 4, 5, 76, 5, 82:1:600]
     iri_data, iri_parameters = calculate_iri_data(;
                                                   year = PARAMETERS[1],
@@ -37,13 +37,13 @@ end
     @test !isnothing(file_to_load)
 end
 
-@testitems "Loading" begin
+@testitem "Loading" begin
     iri_file = find_iri_file()
     load_iri(iri_file)
     @test true
 end
 
-@testitems "Interpolating" begin
+@testitem "Interpolating" begin
     iri_file = find_iri_file()
     iri = AURORA.load_iri(iri_file)
     h_atm = make_altitude_grid(60, 500)
